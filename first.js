8e32055a7b3d824708931786417e4399b8acbd9e
Vishal Choudhary <Vishl42970@gmail.com>
2025-03-25 19:38:02 +0530

    succesfull

--- first.js
+++ first.js
@@ -4,6 +4,9 @@ console.log(button);
  const login = "login page added";
  console.log(login);
 
+ const footer = "footer added in our website";
+ console.log(footer);
+
  const payment = "integrated the payment gateway";
  console.log(payment);
 
