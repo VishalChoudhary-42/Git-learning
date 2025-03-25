7a0dc560e7c607b5792c50ef5a70be88801556d7
Vishal Choudhary <Vishl42970@gmail.com>
2025-03-25 20:06:55 +0530

    latest changes

--- first.js
+++ first.js
@@ -4,11 +4,4 @@ console.log(button);
  const login = "login page added";
  console.log(login);
 
- const footer = "footer added in our website";
- console.log(footer);
-
- const payment = "integrated the payment gateway";
- console.log(payment);
-
- const upi = "integrated upi also";
- console.log(upi);
+ console.log("latest changes");
